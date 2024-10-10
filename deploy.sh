70397279c7894603033c64b5fb4a2c9ae382d00e
PritamBansode13 <pritambansode13@gmail.com>
2024-10-10 15:53:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
- 
-# deploy.sh
- 
 set -e  # Exit immediately if a command exits with a non-zero status.
- 
+
 # Define AWS variables
-AWS_ACCOUNT_ID="314806948291"
+AWS_ACCOUNT_ID="314806948298"
 AWS_REGION="us-east-2"
 AWS_REPOSITORY="lambda"
- 
+
 echo "Pushing the Docker image to Amazon ECR..."
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:latest
- 
+
 echo "Creating/Updating Lambda function..."
 aws lambda create-function \
   --function-name randmalay \
